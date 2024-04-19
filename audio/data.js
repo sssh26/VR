94c5d787addd9957248930f8c13f3abc8c9cd624
sssh26 <161790142+sssh26@users.noreply.github.com>
2024-04-19 11:57:39 +0800

    sssh VR

---
+++ audio/data.js
@@ -0,0 +1,426 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "松山高中穿堂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.6162080828469776,
+        "pitch": 0.18318679256561055,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7193789328209856,
+          "pitch": 0.04495842361377633,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.7940167742481155,
+          "pitch": 0.1346072569265253,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.216359347074526,
+          "pitch": 0.13869187586457343,
+          "title": "松山高中活動中心",
+          "text": "興建於民國86年，為綜合活動場館，一樓游泳池、三樓為籃，羽球場、五樓籃，羽，網球場。"
+        },
+        {
+          "yaw": 0.9400825436027489,
+          "pitch": 0.14541937741185507,
+          "title": "松山高中體育組",
+          "text": "松山高中的體育組"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "松山高中體育組旁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 1.0151404833028224,
+        "pitch": 0.09413765729065915,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0728977949476555,
+          "pitch": 0.046119064145429434,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        },
+        {
+          "yaw": -3.0539934355102467,
+          "pitch": 0.08200388087495725,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.13950413023816566,
+          "pitch": 0.08727002364377512,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8480740654941457,
+          "pitch": 0.07060170086126938,
+          "title": "松山高中體育組",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">松山高中的體育組</span>"
+        },
+        {
+          "yaw": 2.233480414889737,
+          "pitch": 0.03385191663855025,
+          "title": "松山高中活動中心",
+          "text": "興建於民國86年，為綜合活動場館，一樓游泳池、三樓為籃，羽球場、五樓籃，羽，網球場。"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "松山高中操場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 1.0146515466466948,
+        "pitch": -0.10926342330162164,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7221041152230896,
+          "pitch": -0.0736185779193903,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.4516812520954634,
+          "pitch": -0.05508167924332241,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.2889822954485002,
+          "pitch": 0.05467526977165882,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.407719594794493,
+          "pitch": 0.0776832661979796,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.8262634207267574,
+          "pitch": 0.042775023088299946,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.25529549139263175,
+          "pitch": -0.07736830261749361,
+          "title": "司令台",
+          "text": "司令台"
+        },
+        {
+          "yaw": 1.2420811396740756,
+          "pitch": -0.034249948845074485,
+          "title": "升旗台",
+          "text": "升旗台"
+        },
+        {
+          "yaw": 2.484827836161463,
+          "pitch": -0.048332488527845996,
+          "title": "籃球場",
+          "text": "籃球場"
+        },
+        {
+          "yaw": -2.85739316503874,
+          "pitch": 0.009208633342627337,
+          "title": "松山高中 羽球及網球場",
+          "text": "可以打羽球或網球"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "松山高中校園景觀",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 2.6931981111948016,
+        "pitch": -0.08961042189693558,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6139381385980895,
+          "pitch": 0.35220854020323955,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6412261245270017,
+          "pitch": -0.05224261892609228,
+          "title": "教學區",
+          "text": "有許多教室"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "松山高中地下停車場入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5138097938285364,
+        "pitch": 0.39944228927136294,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6755975925112034,
+          "pitch": 0.3419866264203062,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.061880418030599,
+          "pitch": 0.10276560511454669,
+          "rotation": 5.497787143782138,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5458167162266765,
+          "pitch": 0.3004739990409835,
+          "title": "松山高中地下停車場",
+          "text": "停車的地方"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "松山高中學生在運動",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.674103218994155,
+        "pitch": 0.030531132094271385,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9817321990005912,
+          "pitch": 0.1673324179482094,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.0031472637353111566,
+          "pitch": 0.33762045006687913,
+          "rotation": 0,
+          "target": "6-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-2",
+      "name": "松山高中學生在運動2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3929034267292995,
+        "pitch": -0.10958905396583951,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7488272619689953,
+          "pitch": 0.21057309939225632,
+          "rotation": 5.497787143782138,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.05894322314079403,
+          "pitch": 0.39379701482054763,
+          "rotation": 0,
+          "target": "7-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-3",
+      "name": "松山高中學生在運動3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.2128887809691147,
+        "pitch": -0.024238339874587567,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4483226579462087,
+          "pitch": 0.45880830892193813,
+          "rotation": 1.5707963267948966,
+          "target": "6-2"
+        },
+        {
+          "yaw": 2.0805397976320448,
+          "pitch": 0.44718462504218337,
+          "rotation": 4.71238898038469,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.6526931071148017,
+          "pitch": 0.3733961419517762,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.627116039833073,
+          "pitch": 0.2629978384042584,
+          "rotation": 0,
+          "target": "6-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "松山高中內部",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "松山高中校門外大路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.1771459799279835,
+        "pitch": -0.015271845306013887,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.2490823875140844,
+          "pitch": -0.18540038779886636,
+          "title": "松山高中",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "松山高中首頁",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
