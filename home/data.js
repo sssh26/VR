f349387e5fad0bc32c8553911b2915b56e8d1d0b
sssh26 <161790142+sssh26@users.noreply.github.com>
2024-05-03 11:29:03 +0800

    sssh vr home

--- home/data.js
+++ home/data.js
@@ -34,7 +34,7 @@ var APP_DATA = {
           "yaw": -1.2490823875140844,
           "pitch": -0.18540038779886636,
           "title": "松山高中",
-          "text": "松山高中"
+          "text": "地址:<div>台北市信義區基隆路一段156號</div><div>學區:台北市信義區<br><div><div>創立:1958年</div><div>創辦日期：<br></div><div><div>1958年台北市立松山初級中學</div><div>1968年台北市立松山國民中學&nbsp;</div><div>1989年台北市立松山高級中學<br></div></div><div><div>代表色:綠色</div><div>學校類型:男女混合教育</div></div></div></div><div>教育部學校代碼：323301<br></div><div>校園面積:30149平方公尺</div><div>校訓：</div><div>倫理、民主、科學、</div><div>榮譽、責任、服務<br></div>"
         }
       ]
     }
